2ef2d10c5334be32224735b5f80fa417e31c1ea2
Pol Lamothe <lamothepol@gmail.com>
2024-11-13 01:04:07 +0100

    starting to create web presentation of the project

--- numberDetection/numberDetectionTools.py
+++ numberDetection/numberDetectionTools.py
@@ -3,6 +3,7 @@ import pickle
 import sys
 sys.path.append("../")
 from classe import *
+import os 
 
 #load MNIST from server
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -14,8 +15,15 @@ x_train = x_train.astype('float32')
 x_train /= 255
 
 def getNetwork():
-    return Networks([28*28,150,10],activation=sigmoid,learningRate=0.1)
+    return Networks([28*28,150,100,10],neuroneActivation=[sigmoid,sigmoid,softmax],learningRate=0.1)
 
-def getTrainedNetwork():
-    with open("numberDetection.pkl", "rb") as file:
+def getTrainedNetwork() -> Networks:
+    with open(os.path.dirname(os.path.realpath(__file__))+"/numberDetection.pkl", "rb") as file:
         return pickle.load(file)
+
+def getTestData() -> dict[np.array]:
+    choice = random.randint(0,1000)
+    return dict({"data":x_test[choice].tolist(),"rightAnswer":y_test[choice].tolist()})
+
+def getNetworkAnswer(input : np.array) -> list[float]:
+    return getTrainedNetwork().forward(input)
---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+import numberDetection.numberDetectionTools
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+# Endpoint GET
+@app.route('/numberDetection/getData', methods=['GET'])
+def post_data():
+    data = numberDetection.numberDetectionTools.getTestData()
+    return dict({"data":data,"agentAnswer":numberDetection.numberDetectionTools.getNetworkAnswer(np.append([],data["data"])).tolist()}), 200
+
+# Lancer le serveur
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
