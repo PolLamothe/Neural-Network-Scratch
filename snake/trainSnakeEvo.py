419c581c4f568f3097afcf4b43bcb4b32f83d5f3
Pol Lamothe <lamothepol@gmail.com>
2024-11-21 09:45:19 +0100

    trying other config

--- snake/trainSnakeEvo.py
+++ snake/trainSnakeEvo.py
@@ -2,8 +2,35 @@ import sys
 sys.path.append("../")
 import classe
 import trainSnakeEvoTools
+import argparse
+import pickle
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", dest="help", action='store_true')
+parser.add_argument("-s", dest="save", action='store_true')
+parser.add_argument("-f",dest="fullMap", action="store_true")
+parser.add_argument("-a",dest="aim")
+
+args = parser.parse_args()
+if(args.help):
+    print("-c : Get all command")
+    print("-s : Save the model once it has finish training")
+    print("-f : Give all of the grid to the IA")
+    print("-a : The average length you want the IA to reach")
+    exit(0)
 
 gameSize = 5
 
-train = trainSnakeEvoTools.trainSnakeEvo(gameSize,20,[75],classe.sigmoid)
-train.train()
+snakeTrain = trainSnakeEvoTools.trainSnakeEvo(gameSize,args.aim,[160],classe.sigmoid)
+snakeTrain.train()
+
+if(args.save):
+    print("saving your model")
+    file_name = './model/snake_'+str(gameSize)
+    file_name+="_FullMap"
+    file_name+=".pkl"
+    with open(file_name, 'wb') as file:
+        pickle.dump(snakeTrain.network, file)
+
+os._exit(0)
--- snake/trainSnakeEvoTools.py
+++ snake/trainSnakeEvoTools.py
@@ -5,14 +5,17 @@ import random
 import operator
 import matplotlib.pyplot as plt
 
-SELECTIONSIZE = 41 #21 because when selecting the 21 best agent we get 210 childs so it's a 10% selection rate
+SELECTIONSIZE = 21 #The number of snake in survivor
+
+MULTIPLIER = 10 #The number of snake that will be generated during the first generation
 
 class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
     def __init__(self, gameSize: int, averageAim: int, hiddenLayers: list[int] = [], activationFunction: callable = None, neuroneActivation: list = None) -> None:
         self.gameSize = gameSize
         self.averageAim = averageAim
         self.childPerformance = []
-        self.child = [classe.Networks([gameSize**2*3]+hiddenLayers+[4],activation=activationFunction,learningRate=0.1,neuroneActivation=neuroneActivation) for i in range(SELECTIONSIZE*10)]
+        #self.child = [classe.Networks([gameSize**2*3]+hiddenLayers+[4],activation=activationFunction,learningRate=0.1,neuroneActivation=neuroneActivation) for i in range(SELECTIONSIZE*MULTIPLIER)]
+        self.child = [classe.Networks([(((gameSize*2)-1)**2-1)*2]+hiddenLayers+[4],activation=activationFunction,learningRate=0.1,neuroneActivation=neuroneActivation) for i in range(SELECTIONSIZE*MULTIPLIER)]
         self.parents = []
         self.hiddenLayers = hiddenLayers
         self.activationFunction = activationFunction
@@ -30,19 +33,22 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
                 print("currentGeneration : "+str(currentGeneration)+" maxLength : "+str(survivor[0]["performance"])+" averageLength : "+str(trainSnakeEvo.getAveragePerformance(survivor)),end="\r")
             self.childPerformance = []
             for i in range(len(self.child)):#benchmarking the childs
-                self.childPerformance.append({"number" : (currentGeneration-1)*SELECTIONSIZE*10+i,"performance":self.__runChild(self.child[i]),"network":self.child[i]})
+                self.childPerformance.append({"number" : (currentGeneration-1)*SELECTIONSIZE*MULTIPLIER+i,"performance":self.__runChild(self.child[i]),"network":self.child[i]})
             self.childPerformance.sort(reverse=True,key=operator.itemgetter("performance"))
             survivor += self.childPerformance[:SELECTIONSIZE]
             survivor.sort(reverse=True,key=operator.itemgetter("performance"))
             survivor = survivor[:SELECTIONSIZE]
             if(not filter(lambda x : x["performance"] >= self.averageAim,survivor)):break
             tempNetworks = []
-            for i in range(len(survivor)):#creating the next childs
-                for x in range(i+1,len(survivor)):
+            for i in range(1,len(survivor)):#creating the next childs
+                for x in range(i,len(survivor)):
                     tempNetworks.append(classe.Networks([self.gameSize**2*3]+self.hiddenLayers+[4],activation=self.activationFunction,learningRate=0.1,neuroneActivation=self.neuroneActivation,parents=[survivor[i]["network"],survivor[x]["network"]]))
             self.child = tempNetworks.copy()
             self.iterationData.append({"generation":currentGeneration,"maxLength":survivor[0]["performance"],"averageLength":trainSnakeEvo.getAveragePerformance(survivor)})
-            plt.plot([i for i in range(len(self.iterationData))],[self.iterationData[i]["averageLength"] for i in range(len(self.iterationData))])
+            plt.plot([i for i in range(len(self.iterationData))],[self.iterationData[i]["averageLength"] for i in range(len(self.iterationData))], label="AverageLength of the selected agent")
+            plt.plot([i for i in range(len(self.iterationData))],[self.iterationData[i]["maxLength"] for i in range(len(self.iterationData))],label = "AverageLength of the best agent")
+            plt.xlabel("generation")
+            plt.ylabel("snake length")
             plt.savefig('evoData.png')
             currentGeneration += 1
 
@@ -59,7 +65,7 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
             game = snakeGame.Game(self.gameSize)
             moveSinceLastFood = 0
             while(state == None):
-                Networkinput = snakeTrainTools.snakeTrainTools.generateInput(game.getGrid(),True,game.snake)
+                Networkinput = trainSnakeEvo.generateInput(game.getGrid(),True,game.snake)
                 result = network.forward(Networkinput)
                 answerIndex = random.choice(snakeTrainTools.snakeTrainTools.getAllMaxIndex(self.__superviseAnswer(game,result,network)))
 
@@ -77,6 +83,7 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
                     game.directionY = 0
                 
                 fruitSave = game.fruit.copy()
+                headSave = game.snake[-1].copy()
                 game.update()
                 state = game.checkState()
                 if(game.fruit != fruitSave):
@@ -86,6 +93,16 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
                     network.backward(errors)
                 else:
                     moveSinceLastFood += 1
+                    currentDistance = abs(game.snake[-1][0]-game.fruit[0])+abs(game.snake[-1][1]-game.fruit[1])
+                    previousDistance = abs(headSave[0]-game.fruit[0])+abs(headSave[1]-game.fruit[1])
+                    if(currentDistance < previousDistance):
+                        errors = [0]*4
+                        errors[answerIndex] = (1-result[answerIndex])* 0.5
+                        network.backward(errors)
+                    elif(currentDistance > previousDistance):
+                        errors = [0]*4
+                        errors[answerIndex] = -result[answerIndex]*0.5
+                        network.backward(errors)
                 if(moveSinceLastFood >= self.gameSize**2):
                     state = False
 
@@ -95,7 +112,7 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
     def __superviseAnswer(self,game : snakeGame.Game,result : list[float],network : classe.Networks) -> int:
         gameGrid = game.getGrid()
         modifiedResult = result.copy()
-        errors = [0]*4
+        errors = [i for i in result]*4
         try:
             if(game.snake[-1][1]-1 < 0 or gameGrid[game.snake[-1][1]-1,game.snake[-1][0]] == -1):
                 errors[0] = -result[0]
@@ -113,3 +130,54 @@ class trainSnakeEvo(snakeTrainTools.snakeTrainTools):
             return modifiedResult
         except IndexError:
             print(game.snake[-1])
+    
+    def generateInput(grid : list[list[int]],seeAllMap : bool,snake : list[list[int]]) -> list[int]:
+        networkInput = []
+        snakeHead = snake[-1]
+        gameSize = len(grid)
+        if(True):
+            radius = 1
+            if(seeAllMap):
+                radius = gameSize-1
+            for j in range(2):
+                #If j == 0 we are searching for food
+                #If j == 1 we are searching for danger
+                for i in range(-radius,radius+1):
+                    for x in range(-radius,radius+1):
+                        if(i != 0 or x != 0):
+                            if((snakeHead[1]+i >= 0 and snakeHead[0]+x >= 0 and snakeHead[1]+i < gameSize and snakeHead[0]+x < gameSize)):#if the case is in the grid
+                                if(j == 0):
+                                    if(grid[snakeHead[1]+i][snakeHead[0]+x] == 1):
+                                        networkInput.append(1)
+                                    else:
+                                        networkInput.append(0)
+                                elif(j == 1):
+                                    if(grid[snakeHead[1]+i][snakeHead[0]+x] == -1):
+                                        networkInput.append(1)
+                                    else:
+                                        networkInput.append(0)
+                            elif j == 1:networkInput.append(1)
+                            else:networkInput.append(0)
+        else:
+            for j in range(3):
+                #If j == 0 we are searching for the head
+                #If j == 1 we are searching for food
+                #If = -- 2 we are searching for snake body
+                for i in range(gameSize):
+                    for x in range(gameSize):
+                        if(j == 0):
+                            if([i,x] == snake[-1]):
+                                networkInput.append(1)
+                            else:
+                                networkInput.append(0)
+                        elif(j == 1):
+                            if(grid[i][x] == 1):
+                                networkInput.append(1)
+                            else:
+                                networkInput.append(0)
+                        else:
+                            if(grid[i][x] == -1):
+                                networkInput.append(1)
+                            else:
+                                networkInput.append(0)
+        return networkInput
