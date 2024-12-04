db4a7d45400a929fad9b046455aa6dbe397445b5
Pol Lamothe <lamothepol@gmail.com>
2024-12-04 08:29:35 +0100

    finishing the new method test

--- newMethodTest.py
+++ newMethodTest.py
@@ -7,8 +7,8 @@ def fonction(x):
 def dfonction(x):
 	return x
     
-n = 10#output
-m = 60#input
+n = 2#output
+m = 3#input
    
 W = np.random.rand(n,m)
 
@@ -20,6 +20,7 @@ Y = fonction(np.dot(W,X)+B)
 
 E = np.random.rand(n)
 
+print("verifying the new Weight")
 newW = (W + (E * dfonction(Y))[:,np.newaxis]*X)
 state = True
 for i in range(len(newW)):
@@ -27,6 +28,36 @@ for i in range(len(newW)):
     for x in range(len(newW[i])):
         if(newW[i][x] != madeOneByOne[x]):
             state = False
+if(state):
+    print("it works !")
+else:
+    print("it don't works")
+
+print("verifying the new Bias")
+newB = (B+E*dfonction(Y))
+state = True
+for i in range(len(newB)):
+    madeOneByOne = B[i] + E[i] * dfonction(Y[i])
+    if(newB[i] != madeOneByOne):
+        state = False
+if(state):
+    print("it works !")
+else:
+    print("it don't works")
+
+print("verifying the new Input error")
+newError = (np.dot(E*Y,W*X))
+state = True
+
+madeOneByOne = []
+for i in range(len(newB)):
+    madeOneByOne.append(E[i] * dfonction(Y[i]) * W[i] * X)
+madeOneByOne = np.array(madeOneByOne).sum(axis=0)
+
+for i in range(len(newError)):
+    if(newError[i] != madeOneByOne[i]):
+        state = False
+
 if(state):
     print("it works !")
 else:
