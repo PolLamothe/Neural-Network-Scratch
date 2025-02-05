Fixing Relu function + CNN MNIST around 90% sucess rate in around 25k iterations and fixing test data normalizing

classe.py
@@ -39,11 +39,10 @@ class Tanh(ActivationFunction):
     
 class Relu(ActivationFunction):
     def function(X):
-        X = np.clip(X, -600, 600)
         return np.maximum(X,0)
     
     def derivative(X):
-        return np.maximum(X,0)
+        return np.minimum(X,1)
     
 class NN():
     def forward():
@@ -146,22 +145,27 @@ class ConvolutionalLayer(Layer):
                     error[j]
                 ,mode="valid") * self.learning_rate
 
-            self.B[j] += sum(error[j]) * self.learning_rate
-
+            self.B[j] += error[j].sum(axis=(0,1)) * self.learning_rate
+        
         input_error = []
+        #print("output error",error.mean(),self.depth)
         for i in range(self.depth):
             temp = None
             for j in range(self.kernel_number):
                 if(temp is None):
                     temp = scipy.signal.convolve2d(
                         error[j],
-                        self.K[j][i],mode="full")
+                        np.flip(self.K[j][i], axis=(0, 1)),mode="full")
                 else:
                     temp += scipy.signal.convolve2d(
                         error[j],
-                        self.K[j][i],mode="full")
+                        np.flip(self.K[j][i], axis=(0, 1)),mode="full")
             input_error.append(copy.deepcopy(temp))
-        return np.array(input_error)
+        input_error = np.array(input_error)
+        
+        #print("input error",input_error.mean(),self.depth)
+        #print("kernel",self.K.mean(),self.depth)
+        return input_error
 
 class PoolingLayer(Layer):
     def __init__(self,input_size : int,output_size : int, max_pooling : bool = True,depth : int = 1):

imageDetection/numberDetection.py
@@ -47,7 +47,7 @@ def changeFormat(data : np.ndarray):
 previousAnswerRatio = 0
 previousAnswerCoeff = 0
 
-previousAnswerNumber = len(numberDetectionTools.x_test)
+previousAnswerNumber = 1000
 
 if(not useTrainedModel or args.save):
     trainingState = True
@@ -56,7 +56,7 @@ if(not useTrainedModel or args.save):
     rightCount = dict({})
     for i in range(10):
         rightCount[i] = 0
-    while(previousAnswerRatio < 0.95 or previousAnswerCoeff < previousAnswerNumber):
+    while(previousAnswerRatio < 0.9 or previousAnswerCoeff < previousAnswerNumber):
         count += 1
         currentIndex = random.randint(0,len(numberDetectionTools.x_train)-1)
         right = numberDetectionTools.y_train[currentIndex]
@@ -74,7 +74,7 @@ if(not useTrainedModel or args.save):
             else:
                 err.append(-lastLayerResult[i])
         network.backward(np.array(err))
-        print("number of iterations : "+str(count)," success rate : ",round(previousAnswerRatio,2), end='\r')
+        print("number of iterations : "+str(count)," success rate : ",round(previousAnswerRatio,2),"   ", end='\r')
         sys.stdout.flush()
     print("\ntraining over")
     trainingState = False
@@ -88,7 +88,7 @@ if(not useTrainedModel or args.save):
 successCout = 0
 testCount = 0
 for i in range(len(numberDetectionTools.x_test)):
-    result = network.forward(np.append(numberDetectionTools.x_test[i],[]))
+    result = network.forward(np.array([numberDetectionTools.x_test[i]]))
     right = numberDetectionTools.y_test[i]
     if(checkAnswer(right,result)):
         successCout += 1